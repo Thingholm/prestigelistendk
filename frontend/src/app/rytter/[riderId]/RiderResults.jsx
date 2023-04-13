f940d3f8a6212085ec76210eae567a2caf0cc58a
Rasmus Thingholm <rasmusthingholm@gmail.com>
2023-04-13 12:14:00 +0200

    rider top results component setup

--- frontend/src/app/rytter/[riderId]/RiderResults.jsx
+++ frontend/src/app/rytter/[riderId]/RiderResults.jsx
@@ -1,9 +1,32 @@
+import Link from "next/link";
+
 export default function RiderResults(props) {
     const resultList = props.resultData.sort(function (a, b) { return b.points - a.points });
 
+    const resultUniques = resultList.reduce((list, result) => {
+        const currCount = list[result.raceName] ?? 0;
+        return {
+            ...list,
+            [result.raceName]: currCount + 1,
+        }
+    }, {})
+
+    const seen = new Set();
+
+    const filteredResults = resultList.filter(e => {
+        const duplicate = seen.has(e.raceName);
+        seen.add(e.raceName)
+
+        return !duplicate;
+    })
+
     return (
-        <div>
-            {resultList.map(r => <p>{r.raceName.split(" (")[0]} {r.points}</p>)}
+        <div className="rider-top-results-container">
+            <h4>Største resultater:</h4>
+            {filteredResults.slice(0, 10).map(r =>
+                <p key={r.riderId}>{resultUniques[r.raceName] > 1 && resultUniques[r.raceName] + "x"} {r.raceName.split(" (")[0]} - {r.points}</p>
+            )}
+            <p><Link href="#">Se alle pointgivende resultater...</Link></p>
         </div>
     )
 }
