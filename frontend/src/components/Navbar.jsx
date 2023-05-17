3120df7d333c981404c164b96a03671b4a46e7d7
Rasmus Thingholm <rasmusthingholm@gmail.com>
2023-05-17 16:07:02 +0200

    nav fixes

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,25 +5,25 @@ import { IoChevronDownOutline } from "react-icons/io5"
 export default function Navbar() {
     return (
         <header>
-            <h1><Link href="#">Prestigelisten</Link></h1>
+            <h1><Link href="/">Prestigelisten</Link></h1>
             <nav>
                 <ul>
-                    <li><Link href="#">Om listen</Link></li>
-                    <li><Link href="#">Pointsystem</Link></li>
-                    <li><Link href="#">Quiz</Link></li>
+                    <li><Link href="/om-prestigelisten">Om listen</Link></li>
+                    <li><Link href="/pointsystem">Pointsystem</Link></li>
+                    <li><Link href="https://docs.google.com/spreadsheets/d/14JS3ioc3jaFTDX2wuHRniE3g3S2yyg1QkfJ7FiNgAE8/edit#gid=768297916">Quiz</Link></li>
                     <li className="nav-item-with-dropdown">
-                        <Link href="#">Ranglister <span><IoChevronDownOutline className="down-icon" /></span></Link>
+                        <Link href="/listen" className="dropdown-button">Ranglister <span><IoChevronDownOutline className="down-icon" /></span></Link>
                         <div className="nav-dropdown-menu">
                             <ul>
-                                <li><Link href="#">All time</Link></li>
-                                <li><Link href="#">Aktive ryttere</Link></li>
-                                <li><Link href="#">Nationer</Link></li>
-                                <li><Link href="#">Største danskere</Link></li>
+                                <li><Link href="/listen">All time</Link></li>
+                                <li><Link href="/listen?activeStatus=active">Aktive ryttere</Link></li>
+                                <li><Link href="/nationer">Nationer</Link></li>
+                                <li><Link href="/listen?nation=Danmark">Største danskere</Link></li>
                                 <li><Link href="#">3-årig rullende rangliste</Link></li>
                                 <li><Link href="#">All time hvert år</Link></li>
                                 <li><Link href="#">Største per årti</Link></li>
                                 <li><Link href="#">Største individuelle sæsoner</Link></li>
-                                <li><Link href="#">Største per årgang</Link></li>
+                                <li><Link href="/listen?yearFilterRange=single">Største per årgang</Link></li>
                             </ul>
                         </div>
                     </li>
