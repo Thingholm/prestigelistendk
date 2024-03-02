6dd2d60c5054d4330dba5cdde58bf6e3e07cfe71
Thingholm <rasmusthingholm@gmail.com>
2024-03-02 10:27:57 +0100

    Age select minimum to 14

--- frontend/src/app/alder/AgeSelect.jsx
+++ frontend/src/app/alder/AgeSelect.jsx
@@ -19,7 +19,7 @@ export default function AgeSelect() {
             <label htmlFor="season-select">Vælg alder:</label>
             <select name="season-select" id="season-select" onChange={e => router.push(pathname + "?" + e.target.value)}>
                 {[...Array(40)].map((i, index) => {
-                    const age = index + 12
+                    const age = index + 14
                     return (
                         <option key={index} value={age} selected={searchParams == age && "selected"}>{age}</option>
                     )
